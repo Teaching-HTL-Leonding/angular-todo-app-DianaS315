0776760f127797f7ad3b8e0d9ddfa41a0e0f714d
Diana <dianas@outlook.at>
2022-10-18 10:23:37 +0200

    ngModel added, demo task added

--- ToDo_List/src/app/app.component.ts
+++ ToDo_List/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-
+class Chore{
+    constructor(
+      public title: String,
+      public assignedPerson: (String | undefined), // does not need an assigned person
+      public done: boolean,
+    ){}
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo_List';
+  public chore: Chore;
+
+  constructor(){
+    this.chore = new Chore('clean', 'Dad', false);
+  }
+
 }
--- ToDo_List/src/app/app.module.ts
+++ ToDo_List/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
